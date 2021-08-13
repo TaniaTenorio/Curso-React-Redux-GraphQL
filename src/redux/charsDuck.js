536d816ad7fafe814e99e3df9e9550bf43aa7dcb
TaniaTenorio <taniatenorio@hotmail.com>
2021-08-13 18:02:13 -0500

    [feature] fetch and render characters

--- src/redux/charsDuck.js
+++ src/redux/charsDuck.js
@@ -16,9 +16,11 @@ let GET_CHARACTERS_ERROR = "GET_CHARACTERS_ERROR"
 export default function reducer(state = initialData, action) {
     switch(action.type) {
         case GET_CHARACTERS:
-        case GET_CHARACTERS_SUCCESS:
-            return {...state, array: action.payload}
+            return {...state, fetching: true}
         case GET_CHARACTERS_ERROR:
+            return {...state, fetching: false, error: action.payload}
+        case GET_CHARACTERS_SUCCESS:
+            return {...state, array: action.payload, fetching: false}
         default:
             return state
     }
@@ -27,11 +29,21 @@ export default function reducer(state = initialData, action) {
 // actions
 export function getCharactersAction(){
     return (dispatch, getState) => {
+        dispatch({
+            type: GET_CHARACTERS
+        })
         return axios.get(URL)
             .then(res => {
                 dispatch({
                     type: GET_CHARACTERS_SUCCESS,
                     payload: res.data.results
+                })
+            })
+            .catch(err => {
+                console.log(err);
+                dispatch({
+                    type: GET_CHARACTERS_ERROR,
+                    payload: err.response.message
                 })
             })
     }
