aa74caaf7c8fe4825f47e2103193f59df99479ed
TaniaTenorio <taniatenorio@hotmail.com>
2021-08-16 12:58:04 -0500

    conditionally render login and logout buttons

--- src/components/login/LoginPage.js
+++ src/components/login/LoginPage.js
@@ -1,21 +1,53 @@
 import React from 'react'
 import styles from './login.module.css'
+import { connect } from 'react-redux'
+import { doGoogleLoginAction, logOutAction } from '../../redux/userDuck'
 
-export default function LoginPage() {
+function LoginPage({logOutAction, loggedIn, fetching, doGoogleLoginAction}) {
+
+    function doLogin() {
+        doGoogleLoginAction()
+    }
+    if(fetching) {
+        return <h2>Cargando...</h2>
+    }
+
+    function logOut() {
+        logOutAction()
+    }
+
     return (
         <div className={styles.container}>
-            <h1>
+            {loggedIn ? (
+                 <h1>
+                 Cierra tu sesión
+             </h1>
+            ): (
+                <h1>
                 Inicia Sesión con Google
             </h1>
-            <h1>
-                Cierra tu sesión
-            </h1>
-            <button>
-                Iniciar
+            )}
+           {loggedIn ? (
+               <button onClick={logOut}>
+               Cerrar Sesión
+           </button>
+           ) : (
+            <button onClick={doLogin}>
+            Iniciar
             </button>
-            <button>
-                Cerrar Sesión
-            </button>
+           )}
+           
+            
         </div>
     )
 }
+
+function mapState({user: { fetching, loggedIn,  }}) {
+    return {
+        fetching,
+        loggedIn,
+        
+    }
+}
+
+export default connect(mapState, {doGoogleLoginAction, logOutAction})(LoginPage)
