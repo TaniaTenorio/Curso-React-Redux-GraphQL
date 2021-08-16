e2de728a5fcf012384c24d5bf75b40b989a6e07d
TaniaTenorio <taniatenorio@hotmail.com>
2021-08-16 18:55:44 -0500

    create and handle add favorites action

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,17 +1,24 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
+import { connect } from 'react-redux'
+import {
+  addToFavoritesAction,
+  removeCharacterAction,
+} from '../../redux/charsDuck'
 import Card from '../card/Card'
 import styles from './home.module.css'
-import { connect } from 'react-redux'
-import { removeCharacterAction } from '../../redux/charsDuck'
 
-function Home({ chars, removeCharacterAction }) {
+function Home({ chars, removeCharacterAction, addToFavoritesAction }) {
   function renderCharacter() {
     let char = chars[0]
-    return <Card leftClick={nextCharacter} {...char} />
+    return <Card leftClick={nextCharacter} rightClick={addFav} {...char} />
   }
 
   function nextCharacter() {
     removeCharacterAction()
+  }
+
+  function addFav() {
+    addToFavoritesAction()
   }
 
   return (
@@ -29,4 +36,7 @@ function mapState(state) {
   }
 }
 
-export default connect(mapState, { removeCharacterAction })(Home)
+export default connect(mapState, {
+  removeCharacterAction,
+  addToFavoritesAction,
+})(Home)
--- src/redux/charsDuck.js
+++ src/redux/charsDuck.js
@@ -5,6 +5,7 @@ let initialData = {
   fetching: false,
   array: [],
   current: {},
+  favorites: [],
 }
 let URL = 'https://rickandmortyapi.com/api/character'
 let GET_CHARACTERS = 'GET_CHARACTERS'
@@ -12,10 +13,13 @@ let GET_CHARACTERS_SUCCESS = 'GET_CHARACTERS_SUCCESS'
 let GET_CHARACTERS_ERROR = 'GET_CHARACTERS_ERROR'
 
 let REMOVE_CHARACTER = 'REMOVE_CHARACTER'
+let ADD_TO_FAVORITES = 'ADD_TO_FAVORITES'
 
 // reducer
 export default function reducer(state = initialData, action) {
   switch (action.type) {
+    case ADD_TO_FAVORITES:
+      return { ...state, ...action.payload }
     case REMOVE_CHARACTER:
       return { ...state, array: action.payload }
     case GET_CHARACTERS:
@@ -30,6 +34,16 @@ export default function reducer(state = initialData, action) {
 }
 
 // actions
+export let addToFavoritesAction = () => (dispatch, getState) => {
+  let { array, favorites } = getState().characters
+  let char = array.shift()
+  favorites.push(char)
+  dispatch({
+    type: ADD_TO_FAVORITES,
+    payload: { array: [...array], favorites: [...favorites] },
+  })
+}
+
 export let removeCharacterAction = () => (dispatch, getState) => {
   let { array } = getState().characters
   array.shift()
